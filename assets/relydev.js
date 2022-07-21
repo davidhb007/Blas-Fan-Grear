09f6e7542bd5a4c79682a63e71ec82a73107578d
David <davidhb9402@gmail.com>
2022-07-20 22:11:09 -0500

    Added relydev.js

--- assets/relydev.js
+++ assets/relydev.js
@@ -1,1 +1,7 @@
 // Functions added by David @ RelyDev
+
+let customHeading = $("#relydev-main-h1");
+customHeading.css("background-color", "lightblue");
+customHeading.click(() => {
+    alert("Welcome to the Development Theme");
+});
