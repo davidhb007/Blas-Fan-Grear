9ba82adfe18ea1dae1f4e709fae25e4b5f455d96
David <davidhb9402@gmail.com>
2022-07-20 21:21:12 -0500

    Initial commit

---
+++ assets/custom.js
@@ -0,0 +1,31 @@
+/**
+ * Include your custom JavaScript here.
+ *
+ * We also offer some hooks so you can plug your own logic. For instance, if you want to be notified when the variant
+ * changes on product page, you can attach a listener to the document:
+ *
+ * document.addEventListener('variant:changed', function(event) {
+ *   var variant = event.detail.variant; // Gives you access to the whole variant details
+ * });
+ *
+ * You can also add a listener whenever a product is added to the cart:
+ *
+ * document.addEventListener('product:added', function(event) {
+ *   var variant = event.detail.variant; // Get the variant that was added
+ *   var quantity = event.detail.quantity; // Get the quantity that was added
+ * });
+ *
+ * If you just want to force refresh the mini-cart without adding a specific product, you can trigger the event
+ * "cart:refresh" in a similar way (in that case, passing the quantity is not necessary):
+ *
+ * document.documentElement.dispatchEvent(new CustomEvent('cart:refresh', {
+ *   bubbles: true
+ * }));
+ */
+$(document).ready(function(){
+  $('#shopify-section-template--15173410390193__16310248190e6bd00d #Search').on('click', function(){
+    $('html, body').animate({
+      scrollTop: $('html, body').offset().top,
+    });
+  });
+});
---
+++ assets/relydev.js
@@ -0,0 +1,1 @@
+// Functions added by David @ RelyDev
