7e57f8c23f7d6f5ceeb37cf6f68f7ccc8e24850c
David <davidhb9402@gmail.com>
2022-08-03 12:16:22 -0500

    Finish adding tab funtionality to Team Store page

--- assets/relydev.js
+++ assets/relydev.js
@@ -5,3 +5,26 @@ customHeading.css("background-color", "lightblue");
 customHeading.click(() => {
     alert("Welcome to the Development Theme");
 });
+
+function setNewActiveTab(target) {
+    let contentBodies = document.getElementsByClassName("content-body");
+    for (var contentBody of contentBodies) {
+        contentBody.classList.remove("show-active");
+        console.log(contentBody.id);
+    }
+    for (let tab of tabs) {
+        tab.classList.remove("tab-active");
+    }
+    document.getElementById(target.id).classList.add("tab-active");
+    document
+        .getElementById(target.textContent.trim().concat("-contents"))
+        .classList.add("show-active");
+}
+
+let tabs = document.getElementsByClassName("tab");
+for (var tab of tabs) {
+    tab.addEventListener("click", function (e) {
+        console.log(e.currentTarget);
+        setNewActiveTab(e.currentTarget);
+    });
+}
